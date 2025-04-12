7b0cf97bea1b1f50fe96407c602b66c21005814a
Radoslav Stefanov <radoslav.g.stefanov@gmail.com>
2025-04-13 01:54:23 +0300

    Added v1 of the pairing endpoint logic

---
+++ employees-api/EmployeesApi/Models/EmployeeProject.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeesApi.Models
+{
+    public class EmployeeProject
+    {
+        public int EmpId { get; set; }
+        public int ProjectId { get; set; }
+        public DateTime DateFrom { get; set; }
+        public DateTime DateTo { get; set; }
+    }
+}
