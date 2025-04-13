9d5d5e82af0fd0f78bce4201700761baa8cb0512
Radoslav Stefanov <radoslav.g.stefanov@gmail.com>
2025-04-14 02:28:03 +0300

    Cleaning up the service

--- employees-ui/employees-ui/src/app/services/csvupload.service.ts
+++ employees-ui/employees-ui/src/app/services/csvupload.service.ts
@@ -13,15 +13,12 @@ export class CSVUploadService {
   public uploadCSV(file: File): Promise<any> {
     const formData = new FormData();
     formData.append('file', file, file.name);
-    debugger;
 
     return this.http.post(environment.apiBaseUrl + 'upload', formData).toPromise()
       .then(response => {
-        console.log('CSV uploaded successfully', response);
         return response;
       })
       .catch(error => {
-        console.error('Error uploading CSV', error);
         throw error;
       });
   }
