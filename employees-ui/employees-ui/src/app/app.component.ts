a218c8fda8b3f2c7e7438e34b5c75b115b40d0d9
Radoslav Stefanov <radoslav.g.stefanov@gmail.com>
2025-04-13 23:50:51 +0300

    Added base html for the app

--- employees-ui/employees-ui/src/app/app.component.ts
+++ employees-ui/employees-ui/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CsvUploadComponent } from "./components/csv-upload/csv-upload.component";
+import { AwardDetailsComponent } from './components/award-details/award-details.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CsvUploadComponent, AwardDetailsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
