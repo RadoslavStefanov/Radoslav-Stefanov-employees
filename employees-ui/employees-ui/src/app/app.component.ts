a4fd58d9764946de1c58f0a64c653d3695ac34f1
Radoslav Stefanov <radoslav.g.stefanov@gmail.com>
2025-04-14 01:18:45 +0300

    Redecorated the app component

--- employees-ui/employees-ui/src/app/app.component.ts
+++ employees-ui/employees-ui/src/app/app.component.ts
@@ -2,13 +2,22 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CsvUploadComponent } from "./components/csv-upload/csv-upload.component";
 import { AwardDetailsComponent } from './components/award-details/award-details.component';
+import { NgIf } from '@angular/common';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CsvUploadComponent, AwardDetailsComponent],
+  imports: [RouterOutlet, CsvUploadComponent, AwardDetailsComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'employees-ui';
+  showResultsComponent = false;
+
+  constructor(private toastr: ToastrService) {}
+  
+  ngOnInit() {
+    this.toastr.success('The Employee Management System is at your service!', 'Welcome!', { timeOut: 3000 });
+  }
 }
