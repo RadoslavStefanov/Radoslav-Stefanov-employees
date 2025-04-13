6c452450aa96c265328e2732fd663833f59a128b
Radoslav Stefanov <radoslav.g.stefanov@gmail.com>
2025-04-14 01:19:27 +0300

    Added award details component

---
+++ employees-ui/employees-ui/src/app/components/award-details/award-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-award-details',
+  imports: [],
+  templateUrl: './award-details.component.html',
+  styleUrl: './award-details.component.scss'
+})
+export class AwardDetailsComponent {
+
+}
